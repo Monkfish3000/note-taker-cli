a9608fa861b81d93476e66db21cd60f7dbc196de
Michael <michael@Michaels-Laptop.local>
2024-06-28 16:19:10 +0200

    render notes to browser

--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import http from "node:http";
 import open from "open";
 
-const interpolate = (html, data) => {
+export const interpolate = (html, data) => {
   return html.replace(/\{\{\s*(\w+)\s*\}\}/g, (match, placeholder) => {
     return data[placeholder] || "";
   });
 };
 
-const formatNotes = (notes) => {
+export const formatNotes = (notes) => {
   return notes
     .map((note) => {
       return `
         <div class='note'>
-            <p${note.content}</p>
+            <p>${note.content}</p>
             <div class="tags">
-            ${note.tag.map((tag) => `<span class='tag'>${tag}</span>`)}
+            ${note.tags
+              .map((tag) => `<span class='tag'>${tag}</span>`)
+              .join("")}
             </div>
         </div>
         `;
@@ -34,11 +36,11 @@ const createServer = (notes) => {
   });
 };
 
-const start = (notes, port) => {
+export const start = (notes, port) => {
   const server = createServer(notes);
+  const address = `http://localhost:${port}`;
   server.listen(port, () => {
-    const address = `http://localhost:${post}`;
     console.log(`Server is listening on ${address}`);
-    open(address);
   });
+  open(address);
 };
